6cc89a5e14c1086abef4b229894fa74ab09f9cb2
bazarjansent <178275932+bazarjansent@users.noreply.github.com>
2025-05-07 21:09:22 +0800

    Update footer.php

--- includes/includes/includes/footer.php
+++ includes/includes/includes/footer.php
@@ -1,5 +1,10 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
 <footer>
-    <p>&copy; 2025 Honda Dealership. All rights reserved.</p>
+    <p>© 2025 Honda Dealership. All rights reserved.</p>
 </footer>
 </body>
 </html>
